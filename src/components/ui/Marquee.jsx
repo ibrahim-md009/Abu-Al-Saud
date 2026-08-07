6fbd776d9d6941c13f2b9800cc9ce4495f723445
ibrahim-md009 <ibrahim.shubair.dev@gmail.com>
2026-08-08 01:29:19 +0300

    fix marquee

--- src/components/ui/Marquee.jsx
+++ src/components/ui/Marquee.jsx
@@ -1,25 +1,26 @@
+import React from "react";
+import data from "../../data/products.json";
+
+const orientalSubCategoryIds = data.subCategories
+  .filter((sub) => sub.mainCategoryId === "oriental")
+  .map((sub) => sub.id);
+
+const orientalProducts = data.products.filter((p) =>
+  orientalSubCategoryIds.includes(p.subCategoryId),
+);
+console.log("Oriental products count:", orientalProducts.length);
+console.log(orientalProducts.map((p) => p.name));
+
 const Marquee = () => {
   return (
     <div className="marquee-wrap">
       <div className="marquee-track">
-        <span>كنافة نابلسية</span>
-        <span className="dot">◆</span> <span>بقلاوة بالفستق</span>
-        <span className="dot">◆</span>
-        <span>عش البلبل باللوز</span>
-        <span className="dot">◆</span> <span>بورمة قشطة</span>
-        <span className="dot">◆</span>
-        <span>كنافة اسطنبولية</span>
-        <span className="dot">◆</span> <span>نمورة بالقشطة</span>
-        <span className="dot">◆</span>
-        <span>كنافة نابلسية</span>
-        <span className="dot">◆</span> <span>بقلاوة بالفستق</span>
-        <span className="dot">◆</span>
-        <span>عش البلبل باللوز</span>
-        <span className="dot">◆</span> <span>بورمة قشطة</span>
-        <span className="dot">◆</span>
-        <span>كنافة اسطنبولية</span>
-        <span className="dot">◆</span> <span>نمورة بالقشطة</span>
-        <span className="dot">◆</span>
+        {[...orientalProducts, ...orientalProducts].map((p, i) => (
+          <React.Fragment key={`${p.id}-${i}`}>
+            <span>{p.name}</span>
+            <span className="dot">◆</span>
+          </React.Fragment>
+        ))}
       </div>
     </div>
   );
